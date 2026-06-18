8317e7b7f7e8da375fb707755260c8e369f35f9b
Shinkarna <70016672+Shinkarna@users.noreply.github.com>
2026-06-18 19:05:22 +0800

    Fix wrench not working
    - Add data/forge/tags/items/tools/wrenches.json to register pipez_ce:wrench
      in the standard Forge 1.20.1 wrench tag
    - Fix ModItemTags WRENCH_TAG from forge:tools/wrench to forge:tools/wrenches
      (correct Forge 1.20.1 tag naming convention)

--- src/main/java/com/nekogan/pipez/tags/ModItemTags.java
+++ src/main/java/com/nekogan/pipez/tags/ModItemTags.java
@@ -9,7 +9,7 @@ import net.minecraft.world.item.Item;
 public class ModItemTags {
 
     public static final TagKey<Item> WRENCHES_TAG = ItemTags.create(new ResourceLocation("forge", "wrenches"));
-    public static final TagKey<Item> WRENCH_TAG = ItemTags.create(new ResourceLocation("forge", "tools/wrench"));
+    public static final TagKey<Item> WRENCH_TAG = ItemTags.create(new ResourceLocation("forge", "tools/wrenches"));
     public static final TagKey<Item> TOOLS_TAG = ItemTags.create(new ResourceLocation("forge", "tools"));
     public static final TagKey<Item> UPGRADES_TAG = ItemTags.create(new ResourceLocation(Main.MODID, "upgrades"));
 
